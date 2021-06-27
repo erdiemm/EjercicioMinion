a88456d5c0eba542a78b93882c7a976a85183ca6
Daniel Mendez <erdiemm@hotmail.com>
2021-06-26 22:43:50 -0300

    Agregando Minions

--- src/main/kotlin/ar/edu/unahur/obj2/minions/Minion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/minions/Minion.kt
@@ -1,2 +1,17 @@
 package ar.edu.unahur.obj2.minions
 
+abstract class Minion() {
+    var estamina = 0
+    abstract fun agregarEstamina(valor: Int)
+}
+
+class Biciclopes(): Minion() {
+    override fun agregarEstamina(valor: Int) = if (valor >=10){estamina = 10} else {estamina = valor}
+}
+
+class ciclopes(): Minion(){
+    override fun agregarEstamina(valor: Int) {
+        estamina = valor
+    }
+
+}
